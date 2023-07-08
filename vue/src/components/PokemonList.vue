d16e497cc09e95287e4cf7edb12e6072b6d26733
kubra <kubracetin.ist@gmail.com>
2023-07-08 16:50:17 -0400

    view

---
+++ vue/src/components/PokemonList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'pokemon-list',
+   data() {
+      return {
+        pokemonArray : []
+      }
+   }
+}
+</script>
+
+<style>
+
+</style>
