1bd65b1bf2d8fcd288598040d30b6348f46b1716
kubra <kubracetin.ist@gmail.com>
2023-07-12 18:05:34 -0400

    pokemonList

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'pokemon' }">List all Pokemon</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'logout' }" 
+      v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
--- vue/src/components/PokemonList.vue
+++ vue/src/components/PokemonList.vue
@@ -1,17 +1,29 @@
 <template>
     <div>
-
+<ul>    
+  <li v-for = "pokemon in pokemonArray" v-bind:key="pokemon.id">
+{{ pokemon.name }}</li>
+</ul>
     </div>
   
 </template>
 
 <script>
+import pokemonService from '../services/PokemonService.js';
 export default {
   name: 'pokemon-list',
    data() {
       return {
         pokemonArray : []
       }
+   },
+   created() {
+    // use a service to query the backend
+    pokemonService.getAllPokemon() //this returns a promise
+    .then(response => {
+       console.log(response);
+       this.pokemonArray = response.data
+    })
    }
 }
 </script>
