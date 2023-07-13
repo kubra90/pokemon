26143b134b4f7e5cf9798e357b589c12a2194388
kubra <kubracetin.ist@gmail.com>
2023-07-13 16:25:55 -0400

    java update

--- java/src/main/java/com/techelevator/dao/JdbcPokemonDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcPokemonDao.java
@@ -2,11 +2,20 @@ package com.techelevator.dao;
 
 import com.techelevator.model.PokemonDTO;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class JdbcPokemonDao implements PokemonDao{
     private JdbcTemplate template;
+
+    public JdbcPokemonDao(JdbcTemplate template) {
+        this.template = template;
+    }
+
     @Override
     public void savePokemon(PokemonDTO dto) {
-
+      String sql = "INSERT INTO pokemon (api_id, name, url) VALUES (?, ?, ?)";
+      template.update(sql, dto.getApiId(), dto.getName(), dto.getUrl());
     }
 }
